64b6f867b00b2741242a036d9bbe5acfed02298b
anshuldutt21 <anshuldutt21@gmail.com>
2020-09-16 23:38:30 +0530

    add signals for subscription of new users and categories

--- signals/__init__.py
+++ signals/__init__.py
@@ -2,3 +2,11 @@ from notifications.signals.subscription import (
     subscription_handler,
     unsubscription_handler
 )
+
+from notifications.signals.subscription_all_categories import (
+    subscription_all_categories
+)
+
+from notifications.signals.subscription_all_persons import (
+    subscription_all_persons
+)
